3cdef9c1ee00ef5f2a15c3fc98abbeffef8f1eb8
KamoliddinAbduxalikov <abduxalikovkamoliddin02@gmail.com>
2024-05-06 21:20:11 +0500

    Initial commit

---
+++ src/main/java/org/example/WorkTelegramBotProjectApplication.java
@@ -0,0 +1,12 @@
+package org.example.worktelegrambotproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkTelegramBotProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkTelegramBotProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/org/example/WorkTelegramBotProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.worktelegrambotproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkTelegramBotProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
